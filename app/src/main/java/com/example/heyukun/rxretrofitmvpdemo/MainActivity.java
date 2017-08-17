6b28e2129b1bef709e96aae60b33d2ee6be661ca
heyukun <1412805450@qq.com>
2017-08-17 17:31:53 +0800

    使用 retrofit + rxjava 获取网路数据

--- app/src/main/java/com/example/heyukun/rxretrofitmvpdemo/MainActivity.java
+++ app/src/main/java/com/example/heyukun/rxretrofitmvpdemo/MainActivity.java
@@ -8,11 +8,13 @@ import com.example.heyukun.rxretrofitmvpdemo.entity.Book;
 import com.example.heyukun.rxretrofitmvpdemo.request.RetrofitService;
 import com.google.gson.GsonBuilder;
 
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
 import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
+import rx.Observable;
+import rx.Observer;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity {
     private TextView textView;
@@ -25,20 +27,31 @@ public class MainActivity extends AppCompatActivity {
                 .baseUrl("https://api.douban.com/v2/")
                 .addConverterFactory(GsonConverterFactory.
                         create(new GsonBuilder().create()))
+                .addCallAdapterFactory(RxJavaCallAdapterFactory.create())//支持Rxjava
                 .build();
         RetrofitService service = retrofit.create(RetrofitService.class);
-        Call<Book> call = service.getSearchBook("三国演义",null,0,1);
-        call.enqueue(new Callback<Book>() {
-            @Override
-            public void onResponse(Call<Book> call, Response<Book> response) {
-                textView.setText(response.body()+"");
-            }
+        Observable<Book> observable = service.getSearchBook("三国演义",null,0,1);
+        observable.subscribeOn(Schedulers.io()) //请求数据的事件发生在io线程
+                  .observeOn(AndroidSchedulers.mainThread()) //完成请求后在主线程更新UI
+                  .subscribe(new Observer<Book>() { //订阅
+                      @Override
+                      public void onCompleted() {
+                          //所有事件都完成
+                      }
 
-            @Override
-            public void onFailure(Call<Book> call, Throwable t) {
+                      @Override
+                      public void onError(Throwable e) {
+                          //请求过程中发生错误
+                          e.printStackTrace();
+                      }
 
-            }
-        });
+                      @Override
+                      public void onNext(Book book) {
+                         //请求返回的实例（Book）
+                          textView.setText(book.toString());
+                      }
+                  });
+
 
     }
 }
--- app/src/main/java/com/example/heyukun/rxretrofitmvpdemo/request/RetrofitService.java
+++ app/src/main/java/com/example/heyukun/rxretrofitmvpdemo/request/RetrofitService.java
@@ -5,6 +5,7 @@ import com.example.heyukun.rxretrofitmvpdemo.entity.Book;
 import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Query;
+import rx.Observable;
 
 /**
  * Created by heyukun on 2017/8/17.
@@ -21,9 +22,9 @@ public interface RetrofitService {
      * eg : https://api.douban.com/v2/book/search?q=金瓶梅&tag=&start=0&count=1
      */
     @GET("book/search")
-    Call<Book> getSearchBook(@Query("q") String name,
-                             @Query("tag") String tag,
-                             @Query("start") int start,
-                             @Query("count") int count);
+    Observable<Book> getSearchBook(@Query("q") String name,
+                                   @Query("tag") String tag,
+                                   @Query("start") int start,
+                                   @Query("count") int count);
 
 }
